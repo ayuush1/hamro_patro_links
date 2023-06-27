9953a4b199a63ba4f7f56c95b5e1a87106c97259
ayashh <benpokharel42@gmail.com>
2023-06-27 13:49:20 +0545

    added anchor tag code

--- src/Practice/Dayone.java
+++ src/Practice/Dayone.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.io.FileOutputStream;
 
@@ -20,15 +21,14 @@ public class Dayone {
         //Initiate the web driver
         WebDriver driver=new ChromeDriver();
 
-        //select the url to be done operation in
-        driver.get("https://www.hamropatro.com/");
-
-
-       //Stringify
         String newUrl = "https://www.hamropatro.com/";
 
+        //select the url to be done operation in
+        driver.get(newUrl);
+
+
         //List the elements denoted by the xpath
-        List<WebElement> anchorTags = driver.findElements(By.xpath("//ul[@id='nav']/li/a"));
+        List<WebElement> anchorTags = driver.findElements(By.xpath("//a"));
 
         // Create a new workbook and sheet
         Workbook workbook = new XSSFWorkbook();
@@ -39,66 +39,76 @@ public class Dayone {
         //select the row to be started from
         int rowNumber = 0;
 
+
+
         //Looping through the elements to perform operation
         for (WebElement anchor : anchorTags) {
 
             //get the href attribute
             String link = anchor.getAttribute("href");
 
-            //Converting the srirng into url
-            URL baseUrl = new URL(newUrl);
-
-            //concatenate two url... to do that base url should be in url format other should be in string
-            URL concatenatedURL = new URL(baseUrl, link);
-
-            System.out.println(concatenatedURL);
-
-            //Create the http connection
-            HttpURLConnection connection = null;
+            if (link != null && !link.isEmpty() && !link.startsWith("javascript:")) {
 
 
-            try {
-                //open connection
-                connection = (HttpURLConnection) concatenatedURL.openConnection();
+                //Converting the srirng into url
+                URL baseUrl = new URL(newUrl);
 
-                //Return the header
-                connection.setRequestMethod("HEAD");
+                //concatenate two url... to do that base url should be in url format other should be in string
+                URL concatenatedURL = new URL(baseUrl, link);
 
-                //Get response code
-                int responseCode = connection.getResponseCode();
+                System.out.println(concatenatedURL);
 
-                //create row and cell
-                Row row = sheet.createRow(rowNumber++);
-                Cell urlCell = row.createCell(0);
-                Cell statusCell = row.createCell(1);
-                Cell description = row.createCell(2);
+                //Create the http connection
+                HttpURLConnection connection;
 
 
-                //set value
-                urlCell.setCellValue(concatenatedURL.toString());
-                statusCell.setCellValue(responseCode);
+                try {
+                    //open connection
+                    connection = (HttpURLConnection) concatenatedURL.openConnection();
 
-                if (responseCode >= 400) {
-                    System.out.println("broken link with response status of " + responseCode);
-                    description.setCellValue("the link is broken with the status code of "+responseCode);
+                    //Return the header
+                    connection.setRequestMethod("HEAD");
 
-                } else {
-                    System.out.println("the system is working with status code of " + responseCode);
-                    description.setCellValue("the link is working with the status code of "+responseCode);
+                    //Get response code
+                    int responseCode = connection.getResponseCode();
 
+                    //create row and cell
+                    Row row = sheet.createRow(rowNumber++);
+                    Cell urlCell = row.createCell(0);
+                    Cell statusCell = row.createCell(1);
+                    Cell description = row.createCell(2);
+
+
+                    //set value
+                    urlCell.setCellValue(concatenatedURL.toString());
+                    statusCell.setCellValue(responseCode);
+
+                    if (responseCode >= 400) {
+                        System.out.println("broken link with response status of " + responseCode);
+                        description.setCellValue("the link is broken with the status code of " + responseCode);
+
+                    } else {
+                        System.out.println("the system is working with status code of " + responseCode);
+                        description.setCellValue("the link is working with the status code of " + responseCode);
+
+                    }
+
+                } catch (IOException e) {
+                    System.out.println("the system has error" + e);
                 }
 
-            } catch (IOException e) {
-                System.out.println("the system has error" + e);
+                // Save the workbook
+                FileOutputStream outputFile = new FileOutputStream("C:\\Users\\LENOVO\\Desktop\\hamro_patro\\Report of hamro patro link  working.xlsx");
+                workbook.write(outputFile);
+                outputFile.close();
             }
+            else{
+                System.out.println("the URL is empty");
+            }
+
         }
-////hello
 
 
-        // Save the workbook
-        FileOutputStream outputFile = new FileOutputStream("C:\\Users\\LENOVO\\Desktop\\hamro_patro\\Report of hamro patro link  working.xlsx");
-        workbook.write(outputFile);
-        outputFile.close();
 
         driver.quit();
     }
